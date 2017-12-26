bcc6b2b97b42d32d0f809d913c8f1f1e91d4d280
xubinbin <1323875150@qq.com>
2017-12-26 16:18:38 +0800

    加了注释

--- src/main/java/gds/commondata/freequery/metadata/AbstractFieldBO.java
+++ src/main/java/gds/commondata/freequery/metadata/AbstractFieldBO.java
@@ -1,5 +1,10 @@
 package gds.commondata.freequery.metadata;
-
+/**
+ * 
+ * 字段虚拟类，一切字段类均需继承本类
+ * @author $Author: xubinbin $
+ * @version $Revision:$
+ */
 public abstract class AbstractFieldBO {
 
 	public AbstractFieldBO() {
